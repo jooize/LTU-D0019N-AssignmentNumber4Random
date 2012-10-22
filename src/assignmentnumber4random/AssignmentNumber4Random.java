de37c5703b9e7368726b17521d24db94617146a8
Joel Esko <joel@yuio.se>
2012-10-23 00:30:59 +0200

    Make better

--- src/assignmentnumber4random/AssignmentNumber4Random.java
+++ src/assignmentnumber4random/AssignmentNumber4Random.java
@@ -1,92 +1,21 @@
 package assignmentnumber4random;
-import java.util.Random;
-import java.util.Scanner;
 
 public class AssignmentNumber4Random
 {
-	public static int showMainMenu()
-	{
-		int choices = 3; // Number of menu items
-		int choice = 0; // Selected menu item
-		Scanner input = new Scanner(System.in);
-
-		// Loop until user inputs valid menu item number
-		while(!(choice >= 1 && choice <= choices))
-		{
-			System.out.println("1. New game");
-			System.out.println("2. Statistics");
-			System.out.println("3. End");
-			System.out.println("\nSelect your choice by entering corresponding number.");
-
-			try
-			{
-				choice = input.nextInt(); // Get users choice
-			
-				// Print error if choice is not within range
-				if(!(choice >= 1 && choice <= choices))
-					System.out.println("You must enter one of the options in the list.");
-			}
-			catch(java.util.InputMismatchException e)
-			{
-				System.out.println("You must enter a number only.");
-				input.next();
-			}
-		}
-
-		return choice;
-	}
-
-	public static void playGame()
-	{
-		Random randomGenerator = new Random();
-		Scanner input = new Scanner(System.in);
-		int randomNumber, userNumber = 0;
-
-		randomNumber = randomGenerator.nextInt(100); // Generates 0-99
-		randomNumber++; // Make it 1-100
-
-		System.out.println("A random number has been generated in the range 1-100.");
-		//System.out.println("Number: " + randomNumber);
-
-		while(!(userNumber == randomNumber))
-		{
-			System.out.print("Enter your guess: ");
-			try
-			{
-				userNumber = input.nextInt();
-
-				if(randomNumber > userNumber)
-					System.out.println("Higher.");
-				if(randomNumber < userNumber)
-					System.out.println("Lower.");
-				if(randomNumber == userNumber)
-					System.out.println("Correct!");
-			}
-			catch(java.util.InputMismatchException e)
-			{
-				System.out.println("You must enter a number only.");
-				input.next();
-			}
-		}
-	}
-
-	public static void showStatistics()
-	{
-	}
-
 	public static void main(String[] args)
 	{
 		boolean loop = true;
-		int choice;
+		int menuItemSelected;
+		GuessTheRandomNumber game = new GuessTheRandomNumber();
 
 		while(loop)
 		{
-			choice = showMainMenu();
+			menuItemSelected = game.showMainMenu();
 
-			switch(choice)
+			switch(menuItemSelected)
 			{
-				case 1: playGame(); break;
-				case 2: showStatistics(); break;
+				case 1: game.playGame(); break;
+				case 2: game.showStatistics(); break;
 				case 3: loop = false;
 			}
 		}
---
+++ src/assignmentnumber4random/GuessTheRandomNumber.java
@@ -0,0 +1,92 @@
+package assignmentnumber4random;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheRandomNumber
+{
+	private int gamesPlayed;
+	private int totalGuesses;
+
+	public static int showMainMenu()
+	{
+		Scanner input = new Scanner(System.in);
+		int choice = 0; // Selected menu item
+		int choices = 3; // Number of menu items
+
+		System.out.println();
+		System.out.println("1. New game");
+		System.out.println("2. Statistics");
+		System.out.println("3. End");
+		System.out.println();
+
+		// Loop until user inputs valid menu item number
+		while(!(choice >= 1 && choice <= choices))
+		{
+			System.out.println("Select your choice by entering corresponding number.");
+
+			try
+			{
+				choice = input.nextInt(); // Get users choice
+			
+				// Print error if choice is not within range
+				if(!(choice >= 1 && choice <= choices))
+					System.out.println("You must enter one of the options in the list.");
+			}
+			catch(java.util.InputMismatchException e)
+			{
+				System.out.println("You must enter an integer only.");
+				input.next();
+			}
+		}
+
+		System.out.println();
+		return choice;
+	}
+
+	public void playGame()
+	{
+		Random randomGenerator = new Random();
+		Scanner input = new Scanner(System.in);
+		int randomNumber, userNumber = 0, guesses = 0;
+
+		randomNumber = randomGenerator.nextInt(100); // Generates 0-99
+		randomNumber++; // Make it 1-100
+
+		System.out.println("A random number has been generated in the range 1-100.");
+		//System.out.println("Number: " + randomNumber);
+
+		// Loop until correct guess
+		while(!(userNumber == randomNumber))
+		{
+			System.out.print("Enter your guess: ");
+			try
+			{
+				userNumber = input.nextInt(); // Get players guess
+				guesses++; // Add one guess to statistics
+
+				if(randomNumber > userNumber)
+					System.out.println("Higher.");
+				if(randomNumber < userNumber)
+					System.out.println("Lower.");
+				if(randomNumber == userNumber)
+				{
+					System.out.println("Correct!");
+					this.totalGuesses += guesses; // Add all guesses to total in statistics
+					this.gamesPlayed++; // Add one game played to statistics
+				}
+			}
+			catch(java.util.InputMismatchException e)
+			{
+				System.out.println("You must enter a number only.");
+				input.next();
+			}
+		}
+	}
+
+	public void showStatistics()
+	{
+		System.out.println("Games played: " + this.gamesPlayed);
+		System.out.println("Total guesses: " + this.totalGuesses);
+	}
+}
+
